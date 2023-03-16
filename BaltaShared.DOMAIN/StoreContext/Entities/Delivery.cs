41febfdc703001bf4473039447c3f84c5d1e4445
Josué Diogo Pedro <pedrojosuediogo@gmail.com>
2023-03-16 15:20:40 -0300

    Adicionando o enum do Status do Delivery

--- BaltaShared.DOMAIN/StoreContext/Entities/Delivery.cs
+++ BaltaShared.DOMAIN/StoreContext/Entities/Delivery.cs
@@ -1,9 +1,18 @@
-﻿namespace BaltaShared.DOMAIN.StoreContext.Entities
+﻿using BaltaShared.DOMAIN.StoreContext.Enums;
+
+namespace BaltaShared.DOMAIN.StoreContext.Entities
 {
     public class Delivery
     {
+        public Delivery(DateTime estimateDeliveryDate)
+        {
+            CreateDate = DateTime.Now;
+            EstimateDeliveryDate = estimateDeliveryDate;
+            Status = EDeliveryStatus.Waiting;
+        }
+
         public DateTime CreateDate { get; set; }
         public DateTime EstimateDeliveryDate { get; set; }
-        public string Status { get; set; }
+        public EDeliveryStatus Status { get; set; }
     }
 }
